528bc6b650479393b7d1c43e94d68779ed46fc38
LuizFHSs <devlfhss@gmail.com>
2024-06-20 16:28:25 -0300

    add-main-file

---
+++ main.py
@@ -0,0 +1,26 @@
+import Modules.operacao_bancaria as ob
+
+while True:
+
+    menu = """\tBANCO DIO
+    1 PARA DEPOSITAR
+    2 PARA SACAR
+    3 PARA EXIBIR EXTRATO
+    4 PARA SAIR
+    """
+    print(menu)
+    opcao = int(input("Digite a opção: "))
+
+    if opcao == 1:
+        valor_deposito = float(input("Digite o valor do deposito: "))
+        ob.deposito(valor_deposito)
+    elif opcao == 2:
+        valor_saque = float(input("Digite o valor do saque: "))
+        ob.saque(valor_saque)
+    elif opcao == 3:
+        ob.extrato()
+    elif opcao == 4:
+        break
+    else:
+        print("Opção Inválida!")
+        break;
